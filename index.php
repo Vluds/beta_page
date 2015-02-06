66479490a36e47e661dc1f60ae0ccaeb1ef758bb
TymmeSyde <tymmesyde@gmail.com>
2015-02-06 15:20:39 +0100

    Few Changes

--- index.php
+++ index.php
@@ -7,29 +7,23 @@ define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']), true);
 <html lang="en">
 	<head>
 		<!-- Miscellaneous -->
-			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<meta name="robots" content="none" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="robots" content="none" />
 		
-		<title>mxcmaxime ≡ Something is changing here !</title>
+		<title>Vluds - Découvrez Vluds</title>
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic' rel='stylesheet' type='text/css'>
 		<link href="style.css" rel="stylesheet" type='text/css'>
 
-		<!-- Analytics -->
-			<script>
-				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-				ga('create', 'UA-36957101-1', 'auto');
-				ga('send', 'pageview');
-			</script>
+		<link rel="shortcut icon" href="favicon.ico"/>
 	</head>
 	<body>
 		<div>
-			<img class="logo" src="<?php echo WEBROOT; ?>vluds.png" alt="logo" />
-			<p>Découvrez Vluds_blue la nouvelle version de vluds.eu</p>
+			<div id="logo"></div>
+			<p>
+				Découvrez Vluds_blue la nouvelle version de vluds.eu<br/><br/>
+				'Made with our little hands for you ♥'
+			</p>
 		</div>
 		<div>
 			<img src="<?php echo WEBROOT; ?>preview.png" alt="preview" />
