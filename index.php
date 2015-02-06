b5788f942e5875f0a382da0ae3c93aa010dce614
mxcmaxime <join.mxcmaxime@outlook.fr>
2015-02-06 20:43:15 +0300

    .

--- index.php
+++ index.php
@@ -16,6 +16,16 @@ define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']), true);
 		<link href="style.css" rel="stylesheet" type='text/css'>
 
 		<link rel="shortcut icon" href="favicon.ico"/>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-57154908-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 	</head>
 	<body>
 		<div id="content_top">
